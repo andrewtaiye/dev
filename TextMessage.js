1bcbd76f442c05e6c6d7959af055a7365e64720a
Andrew Tai <andrewtai.ye@gmail.com>
2022-09-11 23:16:40 +0800

    Added typewriter effect

--- TextMessage.js
+++ TextMessage.js
@@ -9,9 +9,14 @@ class TextMessage {
     this.element = document.createElement("div");
     this.element.classList.add("text-message");
     this.element.innerHTML = `
-        <p class="text-message-p">${this.text}</p>
+        <p class="text-message-p"></p>
         <button class="text-message-btn">Next</button>
         `;
+
+    this.revealingText = new RevealText({
+      element: this.element.querySelector(".text-message-p"),
+      text: this.text,
+    });
 
     this.element.querySelector("button").addEventListener("click", () => {
       // Close the text message.
@@ -19,18 +24,23 @@ class TextMessage {
     });
 
     this.actionListener = new KeypressListener("KeyZ", () => {
-      this.actionListener.unbind();
       this.done();
     });
   }
 
   done() {
-    this.element.remove();
-    this.onComplete();
+    if (this.revealingText.isDone) {
+      this.element.remove();
+      this.actionListener.unbind();
+      this.onComplete();
+    } else {
+      this.revealingText.skipToDone();
+    }
   }
 
   init(container) {
     this.createElement();
     container.appendChild(this.element);
+    this.revealingText.init();
   }
 }
